0c4bc9981262f9449dccace8a3529fb58914709c
Sensibelgi294 <sensiveoyorom4218@protonmail.com>
2026-09-05 14:40:43 +0300

    FirstCommit

---
+++ src/component/Layout/Layout.jsx
@@ -0,0 +1,10 @@
+
+import NavbarModern from '../NavbarModern/NavbarModern.jsx';
+
+export default function Layout(props) {
+  return (
+    <>
+      <NavbarModern />
+    </>
+  );
+}
---
+++ src/component/Navbar/NavbarModern.jsx
@@ -0,0 +1,10 @@
+export default function NavbarModern() {
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4 rounded-lg">
+        <h1 className="text-2xl font-bold">Hello World</h1>
+        <p className="mt-2">Welcome to my website</p>
+      </div>
+    </>
+  );
+}
